44f0da2be564b50f72419bc34517bf87cb37430e
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-02-01 19:04:21 -0500

    Update openresolve-sh.sh

--- openresolve-sh.sh
+++ openresolve-sh.sh
@@ -6,7 +6,7 @@ fi
 
 result=$(dig -t TXT $1 +short)
 if [ "$result" = "" ]; then
-  echo "error: no TXT entries"
+  echo "error: no TXT entries" >&2
   exit 1
 fi
-grep -P 'oa1:'"$ticker"' .*?recipient_address=\K.*?(?=;)' -o <<< "$result" || (echo "error: no openalias records found for $ticker" && exit 1)
+grep -P 'oa1:'"$ticker"' .*?recipient_address=\K.*?(?=;)' -o <<< "$result" || (echo "error: no openalias records found for $ticker" >&2 && exit 1)
